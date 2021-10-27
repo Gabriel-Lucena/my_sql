6c79f55ecab04766cb1e23c1782e59c866d8b068
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-10-27 10:48:12 -0300

    Últimos scripts

--- comandos.sql
+++ comandos.sql
@@ -60,3 +60,102 @@ create table tblClassificacao (
 */
 
 show tables;
+
+/*
+
+	Cria uma tabela e seus atributos
+
+*/
+
+create table tblSexo (
+
+	idSexo int not null auto_increment,
+    nomeSexo varchar(10) not null,
+    primary key (idSexo),
+    unique index (idSexo)
+
+);
+
+
+/*
+
+	Permite visualizar a descrição da tabela
+
+*/
+
+desc tblSexo;
+
+/*
+
+	Tabela genero
+
+*/
+
+create table tblGenero (
+
+	idGenero int not null auto_increment primary key,
+    nomeGenero varchar(30) not null,
+    unique index(idGenero)
+
+);
+
+/*
+
+	Tabela estudio
+
+*/
+
+create table tblEstudio (
+
+	idEstudio int not null auto_increment primary key,
+    nomeEstudio varchar(50) not null,
+    unique index (idEstudio)
+
+);
+
+/*
+
+	Tabela nacionalidade
+
+*/
+
+create table tblNacionalidade (
+
+	idNacionalidade int not null auto_increment primary key,
+    nomeNacionalidade varchar(20) not null,
+    unique index(idNacionalidade)
+
+);
+
+/*
+
+	tabela filme
+
+*/
+
+create table tblFilme (
+
+	idFilme int not null auto_increment primary key,
+    nomeFilme varchar(80) not null,
+    nomeOriginalFilme varchar(80),
+	duracao time not null,
+    dataLancamento date not null,
+    dataRelancamento date,
+    sinopse text not null,
+    fotoFilme varchar(80),
+    idClassificacao int not null,
+    
+    /* Defini um nome do relacionamento */
+    
+    constraint FK_Classificacao_Filme
+    
+    /* Especifica quem será a FK nesta tabela */
+    
+    foreign key (idClassificacao)
+    
+    /* Especifica de qual tabela virá a PK */
+    
+    references tblClassificacao (idClassificacao),
+    unique index (idFilme)
+    
+);
